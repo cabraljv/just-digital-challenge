684ca8b95437b4b6cfc9a7f434b6637b6caedc07
cabraljv <joaovictorcabral.dev@gmail.com>
2021-03-02 23:51:56 -0300

    feat: create products page

---
+++ src/pages/Product/index.tsx
@@ -0,0 +1,118 @@
+import React, { useMemo, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { TiShoppingCart } from 'react-icons/ti';
+import NavBar from '../../components/NavBar';
+import api from '../../services/api';
+import { Container } from './styles';
+import HomeProduct from '../../components/HomeProduct';
+import AddProduct from '../../components/AddProduct';
+
+interface IProduct {
+  id: number;
+  picture: string;
+  price: number;
+  title: string;
+  description: string;
+  brand: string;
+  quantity: number;
+}
+
+interface Params {
+  id: string;
+}
+
+interface APIResponse {
+  products: IProduct[];
+}
+
+const Product: React.FC = () => {
+  const [product, setProduct] = useState<IProduct>();
+  const [recomended, setRecomended] = useState<IProduct[]>([]);
+  const [addOpen, setAddOpen] = useState(false);
+
+  const { id } = useParams<Params>();
+  useEffect(() => {
+    async function getData() {
+      const response = await api.get<APIResponse>(`/`);
+      if (response.status !== 200) {
+        // eslint-disable-next-line no-alert
+        alert('Ocorreu um erro ao carregar o produto');
+      }
+      const find_product = response.data.products.find(
+        (item) => `${item.id}` === id
+      );
+      if (find_product) {
+        setProduct(find_product);
+        const recommended_products = response.data.products.filter(
+          (item) => `${item.id}` !== id
+        );
+        setRecomended(recommended_products);
+      }
+    }
+    getData();
+  }, [id]);
+  const formated_price = useMemo(() => {
+    return `${product?.price.toFixed(2)}`.replace('.', ',');
+  }, [product?.price]);
+  return (
+    <>
+      <NavBar />
+      <Container>
+        <div className="content">
+          {product && (
+            <div className="product-data">
+              <img src={product.picture} alt="" />
+              <div className="info">
+                <div className="header">
+                  <h1>{product.title}</h1>
+                  <span>
+                    <p>{product.quantity} disponíveis</p>
+                  </span>
+                </div>
+                <p className="description">{product.description}</p>
+                <div className="footer">
+                  <div className="price">
+                    <span className="price-value">
+                      R$ <span>{formated_price}</span>
+                    </span>
+                  </div>
+                  <button type="button" onClick={() => setAddOpen(true)}>
+                    <TiShoppingCart size={25} />
+                    <p>ADICIONAR AO CARRINHO</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
+          <p className="recomended-text">
+            Outros produtos que talvez você goste
+          </p>
+          <div className="recomended-products">
+            {recomended.map(
+              (item) =>
+                `${item.id}` !== id && (
+                  <HomeProduct
+                    title={item.title}
+                    key={item.id}
+                    picture={item.picture}
+                    price={item.price}
+                    id={item.id}
+                  />
+                )
+            )}
+          </div>
+        </div>
+      </Container>
+      {product && (
+        <AddProduct
+          active={addOpen}
+          product={product}
+          onCancel={() => setAddOpen(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default Product;
---
+++ src/pages/Product/styles.ts
@@ -0,0 +1,128 @@
+import styled from 'styled-components';
+import theme from '../../styles/themes';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div.content {
+    width: 90%;
+    max-width: 1160px;
+    margin: 0 auto;
+    div.product-data {
+      margin: 30px 0;
+      display: flex;
+      justify-content: space-between;
+      img {
+        width: 40%;
+        height: 100%;
+      }
+      @media (max-width: 720px) {
+        flex-direction: column;
+        img {
+          width: 100%;
+          height: 100%;
+          margin-bottom: 10px;
+        }
+      }
+      div.info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding-left: 5%;
+        div.header {
+          h1 {
+            font-weight: 800;
+            font-size: 1.4rem;
+          }
+          span {
+            display: flex;
+            justify-content: space-between;
+            p:first-of-type {
+              font-size: 18px;
+              line-height: 25px;
+            }
+            p:last-of-type {
+              font-size: 14px;
+              color: ${theme.light_text};
+            }
+          }
+          margin-bottom: 20px;
+        }
+        p.description {
+          font-size: 1rem;
+          color: ${theme.light_text};
+        }
+        div.footer {
+          margin-top: auto;
+          display: flex;
+          @media (max-width: 720px) {
+            flex-direction: column;
+          }
+          button {
+            background: none;
+            border: 2px solid ${theme.primary};
+            color: ${theme.primary};
+            font-size: 18px;
+            font-weight: 700;
+            letter-spacing: 0.04em;
+            padding: 15px 25px;
+            border-radius: 15px;
+            transition: background 0.4s;
+            display: flex;
+            align-items: center;
+            p {
+              padding-left: 5px;
+            }
+          }
+          button:hover {
+            background: ${theme.primary};
+            color: #fff;
+            filter: drop-shadow(0px 0px 5px ${theme.primary});
+          }
+          div.price {
+            margin-top: 20px;
+            display: flex;
+            margin-left: auto;
+            margin-right: 80px;
+            span.price-value {
+              font-size: 12px;
+              color: #666;
+              display: flex;
+              span {
+                color: ${theme.primary};
+                font-size: 36px;
+                font-weight: bold;
+                margin-top: -6px;
+                margin-left: 2px;
+              }
+            }
+          }
+        }
+      }
+    }
+    p.recomended-text {
+      margin-top: 90px;
+      margin-bottom: 40px;
+      font-weight: 300;
+      font-size: 36px;
+    }
+    div.recomended-products {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      grid-gap: 30px;
+      div.grid-item {
+        border: 1px solid red;
+        width: 100%;
+        height: 200px;
+      }
+      @media (max-width: 960px) {
+        grid-template-columns: 1fr 1fr 1fr;
+      }
+      @media (max-width: 720px) {
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 5px;
+      }
+    }
+  }
+`;
